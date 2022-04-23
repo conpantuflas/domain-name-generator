dd52ad411af2abbe6932c8ebd0e68e9711d55b97
judith carreño <3280kg@gmail.com>
2022-04-22 20:41:59 -0400

    primer commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//import "bootstrap";
+//import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+let newArray = [];
+
+let aleatorio = function() {
+  for (a = 0; a < pronoun.length; a++) {
+    for (b = 0; b < adj.length; b++) {
+      for (c = 0; c < noun.length; c++) {
+        newArray.push(`${pronoun[a]}${adj[b]}${noun[c]}.com`);
+      }
+    }
+  }
+};
+aleatorio();
+console.log(newArray);
